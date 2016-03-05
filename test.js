cf6aec9b3c933a1935f2ed2b914bff934e987c15
Abhijeet <abhijeetdixit27@gmail.com>
2016-03-05 16:01:12 +0530

    Added package_json. Parsing request body supported. Hard coded URL removed

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var cloudinary = require('cloudinary');
 var express = require('express');
+var bodyparser = require('body-parser');
 
 cloudinary.config({
 	cloud_name : 'drkkz1r25',
@@ -10,49 +11,49 @@ cloudinary.config({
 var app = express();
 app.set('view engine', 'ejs');
 app.use( express.static('public'));
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
 
 app.get('/',function (req, res) {
 	res.render('pages/index');
 });
 
 app.post('/uploadimage',function (req, res) {
-	//var name = req.body.imagepath;
-	//console.log(name);
-	cloudinary.uploader.upload('./public/images/abc.jpg', function (result) {
+	var name = req.body.imagepath;
+	console.log(name);
+	cloudinary.uploader.upload(name, function (result) {
 		console.log(result.result_id);
 		res.send(result.public_id);
 	});
 });
 
 app.post('/uploadimagewithid',function (req, res) {
-	//var name = req.body.imagepath;
-	//var id = req.body.public_id;
-	cloudinary.uploader.upload('./public/images/abc.jpg', function (result) {
+	var name = req.body.imagepath;
+	var id = req.body.public_id;
+	cloudinary.uploader.upload(name, function (result) {
 		res.send(result);
-	}, {public_id : 'sample_image'});
+	}, {public_id : id});
 });
 
 app.post('/uploadvideo',function (req, res) {
-	//var name = req.body.imagepath;
-	//console.log(name);
-	cloudinary.uploader.upload('./public/videos/sample.mp4', function (result) {
+	var name = req.body.imagepath;
+	console.log(name);
+	cloudinary.uploader.upload(name, function (result) {
 		console.log(result.result_id);
 		res.send(result.public_id);
 	}, {resource_type:"video"});
 });
 
 app.post('/uploadvideowithid',function (req, res) {
-	//var name = req.body.imagepath;
-	//var id = req.body.public_id;
-	cloudinary.uploader.upload('./public/videos/sample.mp4', function (result) {
+	var name = req.body.imagepath;
+	var id = req.body.public_id;
+	cloudinary.uploader.upload(name, function (result) {
 		res.send(result);
-	}, {resource_type:"video", public_id : 'sample_video'});
+	}, {resource_type:"video", public_id : id});
 });
 
 app.get('/display', function (req, res) {
-	console.log('I am trying to display an image');
 	res.render('pages/showimage');
-
 });
 
 app.listen(3000, function () {
